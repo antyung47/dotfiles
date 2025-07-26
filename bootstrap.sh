9b9477e45ea0e136eecbe9b80bf725fe62b06ee7
antyung47 <antyung47@gmail.com>
2025-07-26 18:52:12 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
 set -exo pipefail
+
+mkdir -p $HOME/.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh
+
+curl -fsSL https://raw.githubusercontent.com/antyung47/dotfiles/refs/heads/main/user_data/.zshrc.template \
+| envsubst > ~/.zshrc
+
+curl -fsSL https://raw.githubusercontent.com/antyung47/dotfiles/refs/heads/main/user_data/.netrc.template \
+| envsubst > ~/.netrc
+
+mkdir -p $HOME/.aws
+curl -fsSL https://raw.githubusercontent.com/antyung47/dotfiles/refs/heads/main/user_data/.aws/credentials.template \
+| envsubst > $HOME/.aws/credentials
+
+mkdir -p $HOME/.ssh
+curl -fsSL https://raw.githubusercontent.com/antyung47/dotfiles/refs/heads/main/user_data/.ssh/config.template \
+> $HOME/.ssh/config
